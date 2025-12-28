694ff7ada41e454e6d5b14998a8d1f718614b39f
anuj990 <gp712223@gmail.com>
2025-12-28 17:28:52 +0530

    Setted up navigation and MainActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 android {
     namespace = "com.example.notesapp"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.notesapp"
--- app/src/main/java/com/example/notesapp/feature_note/presentation/MainActivity.kt
+++ app/src/main/java/com/example/notesapp/feature_note/presentation/MainActivity.kt
@@ -5,10 +5,22 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.notesapp.feature_note.presentation.add_edit_notes.AddEditNoteScreen
+import com.example.notesapp.feature_note.presentation.notes.NotesScreen
+import com.example.notesapp.feature_note.presentation.util.Screen
 import com.example.notesapp.ui.theme.NotesAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +28,35 @@ class MainActivity : ComponentActivity() {
         setContent {
             NotesAppTheme {
                 Scaffold(modifier = Modifier.Companion.fillMaxSize()) { innerPadding ->
+                    Surface(color = MaterialTheme.colorScheme.background) {
+                        val navController = rememberNavController()
+                        NavHost(
+                            navController = navController,
+                            startDestination = Screen.NotesScreen.route
+                        ) {
+                            composable(
+                                route = Screen.NotesScreen.route
+                            ) {
+                                NotesScreen(navController = navController)
+
+                            }
+                            composable(
+                                route = Screen.AddEditNoteScreen.route + "?noteId = {noteId}&noteColor = {noteColor",
+                                arguments = listOf(
+                                    navArgument(name = "noteId") {
+                                        type = NavType.IntType
+                                        defaultValue = -1
+                                    },
+                                    navArgument(name = "noteColor") {
+                                        type = NavType.IntType
+                                        defaultValue = -1
+                                    })
+                            ) {
+                                val color = it.arguments?.getInt("noteColor")?:-1
+                                AddEditNoteScreen(navController = navController, noteColor = color)
+                            }
+                        }
+                    }
                 }
             }
         }
